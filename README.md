d0259d6f3618e87dc5fb9b1ec50daa95ea52780b
tardisgallifrey <bigmanrunning@gmail.com>
2022-03-06 09:40:09 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice running Strapi calls with Promises
+
+### Some practice working with Strapi Development and React/JavaScript Promises with Fetch.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+//An app, a practice one, to work with
+//Strapi and JavaScript Promises in Fetch
+
+//Will be doing some in functional components,
+//and some in class components
+
+export default function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
